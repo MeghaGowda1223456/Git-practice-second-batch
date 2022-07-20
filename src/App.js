120c2ae5227dcccd53a38adf2bc5dc6bbd5c2be4
pradhyt <pradhy09@gmail.com>
2022-07-20 17:15:58 +0530

    Pradeep uploading

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import DataComponents from "./components/DataComponents";
 import { useState } from "react";
 import ComponentA from "./components/ComponentA";
 import ComponentB from "./components/ComponentB";
+import Pradeep from "./components/Pradeep";
 
 function App() {
   const [state, setState] = useState(true);
   return (
     <div className="App">
+      <Pradeep/>
       <ComponentB/>
       {state ? <DataComponents setState={setState} /> : <ComponentA />}
     </div>
