77fbfc04a632aa0a52a0af3bed399f1a7231cdb6
SuhasK97 <suppik97@gmail.com>
2022-07-20 17:19:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DataComponents from "./components/DataComponents";
 import { useState } from "react";
 import ComponentA from "./components/ComponentA";
 import ComponentB from "./components/ComponentB";
+import Suhas from "./components/Suhas";
 
 function App() {
   const [state, setState] = useState(true);
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <ComponentB/>
       {state ? <DataComponents setState={setState} /> : <ComponentA />}
+      <Suhas/>
     </div>
   );
 }
---
+++ src/components/Suhas.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Suhas() {
+  return (
+    <div>Suhas</div>
+  )
+}
+
+export default Suhas
