401e247307530b344dd165da7d5016334d93ca54
Sunil H R <sunilhr@Sunils-MacBook-Air.local>
2022-07-20 17:17:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import DataComponents from "./components/DataComponents";
 import { useState } from "react";
 import ComponentA from "./components/ComponentA";
 import ComponentB from "./components/ComponentB";
+import Viddd from "./components/Viddd";
 
 function App() {
   const [state, setState] = useState(true);
   return (
     <div className="App">
-      <ComponentB/>
-      {state ? <DataComponents setState={setState} /> : <ComponentA />}
+      {/* <ComponentB/>
+      {state ? <DataComponents setState={setState} /> : <ComponentA />} */}
+      <Viddd/>
+
+
     </div>
   );
 }
